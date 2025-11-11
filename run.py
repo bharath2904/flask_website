95f088389d5e0f8b66c2e9abc1c3fbef769a5375
bharathwajvk03-prog <bharathwaj2904@gmail.com>
2025-11-12 00:00:13 +0530

    Auto create database on startup

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
-from app import create_app
+from app import app, db
 
-app = create_app()
+# ✅ Automatically create tables if not already created
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0")
+    app.run(debug=True)
